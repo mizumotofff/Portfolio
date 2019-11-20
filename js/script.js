8bb5d0aa12418bd1b8cab9f60426736d5ac78032
yutohashiguchi <yuto.hashiguchi@fox-hound.tech>
2019-11-21 05:31:50 +0900

    update

---
+++ js/script.js
@@ -0,0 +1,49 @@
+console.log("test")
+
+
+$(function() {
+    var top = $('.about');
+    var portfolio = $('.portfolio');
+    var contact = $('.contact');
+    var skill = $('.skill');
+    //クリックすると消える
+    top.click(function () {
+        top.addClass("fade");
+    });
+
+    $(window).scroll(function (){
+            var position = $(top).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 500){
+              $(top).addClass('fade');
+            }
+    });
+
+    $(window).scroll(function (){
+            var position = $(portfolio).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 500){
+              $(portfolio).addClass('fade');
+            }
+    });
+
+    $(window).scroll(function (){
+            var position = $(contact).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 500){
+              $(contact).addClass('fade');
+            }
+    });
+
+    $(window).scroll(function (){
+            var position = $(skill).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 500){
+              $(skill).addClass('fade');
+            }
+    });
+});
